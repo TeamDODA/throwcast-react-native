7af77dcbfa642119183510c3701060097e809eab
Ching <chingyang91@gmail.com>
2016-08-10 19:31:35 -0700

    Render play button

--- src/player/playerReducers.js
+++ src/player/playerReducers.js
@@ -4,13 +4,16 @@ const initialState = {
   playing: true,
   muted: false,
   sliding: false,
+  duration: null,
   currentTime: 0,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case "TOGGLE_PLAY":
-
+    case 'TOGGLE_PLAY':
+      return Object.assign({}, state, {
+        playing: !state.playing,
+      });
     default:
       return state;
   }
